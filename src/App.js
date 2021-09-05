89a007863d3887f4e5ddddbfa2a1210cfa56731c
Robin Angel Romero Cordero <ingrobinromeo@gmail.com>
2021-09-05 18:51:31 -0500

    started project with hero design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
+import Hero from "./components/Hero";
+
+// direccion a color tool
+// https://material.io/resources/color/#!/?view.left=0&view.right=0&primary.color=69F0AE&secondary.color=FFB74D&primary.text.color=FB8C00&secondary.text.color=000000
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#9fffe0',
+      main: '#2E4D4D',
+      dark: '#2bbd7e',
+      contrastText: '#fb8c00',
+    },
+    secondary: {
+      light: '#ffe97d',
+      main: '#ffb74d',
+      dark: '#c88719',
+      contrastText: '#000000',
+    },
+    other: {
+      claro: '#FFFFF'
+    }
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <Header></Header>
+        <Hero></Hero>
+      </ThemeProvider>
     </div>
   );
 }
